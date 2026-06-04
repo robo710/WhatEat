eb475cfe8a877ac514544a5dde0d3a3e444b44cc
손찬형 <sonchanhyeong710@gmail.com>
2026-06-04 14:48:13 +0900

    :sparkles: add DevicePreview

---
+++ app/src/main/java/com/example/whateat/presentation/component/DevicePreview.kt
@@ -0,0 +1,10 @@
+package com.example.whateat.presentation.component
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "phone_1", device = "spec:width=360dp,height=880dp,dpi=480")
+@Preview(name = "phone_2", device = "spec:width=412dp,height=732dp,dpi=420")
+@Preview(name = "s23", device = "spec:width=360dp,height=780dp,dpi=480")
+@Preview(name = "foldable", device = "spec:width=673dp,height=841dp,dpi=480")
+@Preview(name = "tablet", device = "spec:width=1280dp,height=800dp,dpi=480")
+annotation class DevicePreviews
