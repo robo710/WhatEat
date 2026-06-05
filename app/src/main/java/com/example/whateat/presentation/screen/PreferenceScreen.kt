f88015083726fc7ab03a6e7d7ab13258fb95b81d
손찬형 <sonchanhyeong710@gmail.com>
2026-06-05 09:15:15 +0900

    :sparkles: create PreferenceScreen

---
+++ app/src/main/java/com/example/whateat/presentation/screen/PreferenceScreen.kt
@@ -0,0 +1,38 @@
+package com.example.whateat.presentation.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import com.example.whateat.presentation.component.DevicePreviews
+import com.example.whateat.ui.theme.WhatEatTheme
+
+@Composable
+fun PreferenceScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFFFFFFF)),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "최초 화면",
+            fontSize = 15.sp
+        )
+    }
+}
+
+@DevicePreviews
+@Composable
+fun PreferenceScreenPreview(){
+    WhatEatTheme {
+        PreferenceScreen()
+    }
+}
